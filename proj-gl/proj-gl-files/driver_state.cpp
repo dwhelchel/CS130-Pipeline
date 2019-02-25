5572a3d87378f74acb27d4012253f9d03437f04c
Dakota <dakotabwhelchel@gmail.com>
2019-02-24 19:30:17 -0800

    think i finished checkpoint 2

--- proj-gl/proj-gl-files/driver_state.cpp
+++ proj-gl/proj-gl-files/driver_state.cpp
@@ -71,7 +71,7 @@ void render(driver_state& state, render_type type)
             dv2.data = new float[state.floats_per_vertex];
             dv3.data = new float[state.floats_per_vertex];
 
-            for (int v = 0; v < state.num_vertices; v += 2) {
+            for (int v = 0; v < state.num_vertices; v += 3) {
                 // Assign appropriate data values from vertex_data into data_vertex
                 for (int i = 0; i < state.floats_per_vertex; ++i) {
                     dv1.data[i] = state.vertex_data[v*state.floats_per_vertex+i];
