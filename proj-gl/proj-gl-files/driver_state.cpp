8ebc9d084d42d4eff1666151982f38dc3b9b0b19
Dakota <dakotabwhelchel@gmail.com>
2019-02-27 19:12:49 -0800

    fixed outer for loop

--- proj-gl/proj-gl-files/driver_state.cpp
+++ proj-gl/proj-gl-files/driver_state.cpp
@@ -72,7 +72,11 @@ void render(driver_state& state, render_type type)
             dv2.data = new float[state.floats_per_vertex];
             dv3.data = new float[state.floats_per_vertex];
 
-            for (int v = 0; v < state.num_vertices * state.floats_per_vertex; v += 3) {
+            double num_triangles = state.num_vertices / 3.0;
+
+            int v = 0;
+
+            for (int j = 0; j < num_triangles; j++, v += 3) {
 
                 // Assign appropriate data values from vertex_data into data_vertex
                 for (int i = 0; i < state.floats_per_vertex; ++i) {
