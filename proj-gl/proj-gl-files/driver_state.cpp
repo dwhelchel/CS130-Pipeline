3c87d09bcdc75e1afdf1753e1d02ebac64e7b91d
Dakota <dakotabwhelchel@gmail.com>
2019-03-11 18:00:27 -0700

    added indexed

--- proj-gl/proj-gl-files/driver_state.cpp
+++ proj-gl/proj-gl-files/driver_state.cpp
@@ -150,15 +150,57 @@ void render(driver_state& state, render_type type)
 
             }
         }
-
         break;
 
         /* Fan */
         case render_type::fan:
+        {
+
+            const data_geometry * geo[3];
+            data_geometry dg1;
+            data_geometry dg2;
+            data_geometry dg3;
+            data_vertex dv1;
+            data_vertex dv2;
+            data_vertex dv3;
+
+        }
         break;
 
         /* Strip */
         case render_type::strip:
+        {
+
+            const data_geometry * geo[3];
+            data_geometry dg1;
+            data_geometry dg2;
+            data_geometry dg3;
+            data_vertex dv1;
+            data_vertex dv2;
+            data_vertex dv3;
+
+            for (int i = 0; i < state.num_vertices - 2; ++i) {
+
+                dv1.data = &state.vertex_data[i * state.floats_per_vertex];
+                dv2.data = &state.vertex_data[(i+1) * state.floats_per_vertex];
+                dv3.data = &state.vertex_data[(i+2) * state.floats_per_vertex];
+                dg1.data = dv1.data;
+                dg2.data = dv2.data;
+                dg3.data = dv3.data;
+
+                state.vertex_shader(dv1, dg1, state.uniform_data);
+                state.vertex_shader(dv2, dg2, state.uniform_data);
+                state.vertex_shader(dv3, dg3, state.uniform_data);
+
+                geo[0] = &dg1;
+                geo[1] = &dg2;
+                geo[2] = &dg3;
+
+                clip_triangle(state, geo, 0);
+
+            }
+
+        }
         break;
     }
 
